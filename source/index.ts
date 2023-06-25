22998a967bb35819562c92a3946271210b8478b0
H2Owater425 <h2o@h2owr.xyz>
2023-06-25 16:18:02 +0900

    feat: Make generateId independent from getting id
    feat: Rename private variables
    refactor: Simplify default export

--- source/index.ts
+++ source/index.ts
@@ -1,79 +1,77 @@
 import { randomInt } from 'crypto';
 
-export interface SnowflakeIdOptions {
+interface SnowflakeIdOptions {
 	epoch?: number;
 	instanceId?: number;
 	overflowHandler?: () => void;
 }
 
-export class SnowflakeId {
-	private id: ArrayBuffer = new ArrayBuffer(8);
-	private idView: DataView = new DataView(this['id']);
+export default class SnowflakeId {
+	private _id: ArrayBuffer = new ArrayBuffer(8);
+	private _idView: DataView = new DataView(this['_id']);
 
-	private lastTime: number = 0;
-	private epoch: number = 0;
-	private sequenceId: number = 0;
-	private instanceId: number = 0;
+	private _lastTime: number = 0;
+	private _epoch: number = 0;
+	private _sequenceId: number = 0;
+	private _instanceId: number = 0;
 
-	private isOverflowHandlerFunction: boolean = false;
-	private overflowHandler: (() => void) | undefined;
+	private _overflowHandler: (() => void) | undefined;
 
 	public static getInstanceId(datacenterId: number, workerId: number): number {
 		if(datacenterId > -1 && datacenterId < 32) {
 			if(workerId > -1 && workerId < 32) {
 				return datacenterId << 5 + workerId;
 			} else {
-				throw new Error('WorkerId should be an integer between 0 and 32');
+				throw new Error('WorkerId must be an integer between 0 and 32');
 			}
 		} else {
-			throw new Error('DatacenterId should be an integer between 0 and 32');
+			throw new Error('DatacenterId must be an integer between 0 and 32');
 		}
 	}
 
 	constructor(options: SnowflakeIdOptions = {}) {
 		if(Number.isInteger(options['epoch'])) {
 			if(options['epoch'] as number <= Date.now()) {
-				this['epoch'] = options['epoch'] as number;
+				this['_epoch'] = options['epoch'] as number;
 			} else {
-				throw new Error('Options[\'epoch\'] should not exceed current time');
+				throw new Error('Options[\'epoch\'] must not exceed current time');
 			}
 		}
 		
 		if(Number.isInteger(options['instanceId'])) {
 			if(options['instanceId'] as number > -1 && options['instanceId'] as number < 1024) {
-				this['instanceId'] = options['instanceId'] as number;
+				this['_instanceId'] = options['instanceId'] as number;
 			} else {
-				throw new Error('Options[\'instanceId\'] should be an integer between 0 and 1023');
+				throw new Error('Options[\'instanceId\'] must be an integer between 0 and 1023');
 			}
 		} else {
-			this['instanceId'] = randomInt(1024);
+			this['_instanceId'] = randomInt(1024);
 		}
 
 		if(typeof(options.overflowHandler) === 'function') {
-			this.overflowHandler = options.overflowHandler;
-			this['isOverflowHandlerFunction'] = true;
+			this._overflowHandler = options.overflowHandler;
 		}
 
-		this['instanceId'] <<= 12;
+		this['_instanceId'] <<= 12;
 	}
 
-	private generateId(): SnowflakeId {
-		if(this['sequenceId'] !== 4096) {
-			const currentTime: number = Date.now() - this['epoch'];
+	public generateId(): SnowflakeId {
+		if(this['_sequenceId'] !== 4096) {
+			const currentTime: number = Date.now() - this['_epoch'];
 	
-			if(this['lastTime'] !== currentTime) {
-				this['sequenceId'] = 0;
+			if(this['_lastTime'] !== currentTime) {
+				this['_sequenceId'] = 0;
 	
-				this['lastTime'] = currentTime;
+				this['_lastTime'] = currentTime;
 			}
 
-			this['idView'].setUint32(0, Math.trunc(currentTime / 1024 /* 2**10 */));
-			this['idView'].setUint32(4, (currentTime << 22) + this['instanceId'] + this['sequenceId']++);
+			this['_idView'].setUint32(0, Math.trunc(currentTime / 1024 /* 2**10 */));
+			this['_idView'].setUint32(4, (currentTime << 22) + this['_instanceId'] + this['_sequenceId']++);
 		} else {
-			if(this['isOverflowHandlerFunction']) {
-				(this.overflowHandler as () => void)();
+			if(typeof(this._overflowHandler) === 'function') {
+				(this._overflowHandler as () => void)();
 			} else {
-				throw new Error('SnowflakeId[\'sequenceId\'] should not exceed 4095 in given millisecond');
+				throw new Error('SnowflakeId[\'sequenceId\'] must not exceed 4095 in given millisecond');
 			}
 		}
 
@@ -81,16 +79,10 @@ export class SnowflakeId {
 	}
 
 	public getId(): BigInt {
-		this.generateId();
-
-		return this['idView'].getBigUint64(0);
+		return this['_idView'].getBigUint64(0);
 	}
 
 	public getBufferId(): Buffer {
-		this.generateId();
-
-		return Buffer.from(this['id']);
+		return Buffer.from(this['_id']);
 	}
 }
-
-export default SnowflakeId;
