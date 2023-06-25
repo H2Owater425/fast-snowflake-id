bfce838a3ef398fbf08056a3597cb9f9f13534d5
H2Owater425 <h2o@h2owr.xyz>
2023-06-25 16:31:13 +0900

    fix: Add SnowflakeIdOptions

--- library/index.d.ts
+++ library/index.d.ts
@@ -1,3 +1,9 @@
+interface SnowflakeIdOptions {
+	epoch?: number;
+	instanceId?: number;
+	overflowHandler?: () => void;
+}
+
 export default class SnowflakeId {
 	public static getInstanceId(datacenterId: number, workerId: number): number;
 	
