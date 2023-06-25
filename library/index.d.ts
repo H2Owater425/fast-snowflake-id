bb72196853f4c8c67a0b1839cffbea5baa62eaad
H2Owater425 <h2o@h2owr.xyz>
2023-06-25 16:22:05 +0900

    chore: Build

--- library/index.d.ts
+++ library/index.d.ts
@@ -1,27 +1,9 @@
-export interface SnowflakeIdOptions {
-	epoch?: number;
-	instanceId?: number;
-	overflowHandler?: () => void;
-}
-
-export class SnowflakeId {
-	private id: ArrayBuffer;
-	private idView: DataView;
-	private lastTime: number;
-	private epoch: number;
-	private sequenceId: number;
-	private instanceId: number;
-	private isOverflowHandlerFunction: boolean;
-	private overflowHandler: (() => void) | undefined;
-
+export default class SnowflakeId {
 	public static getInstanceId(datacenterId: number, workerId: number): number;
 	
 	constructor(options?: SnowflakeIdOptions);
 	
-	private generateId(): SnowflakeId;
-
+	public generateId(): SnowflakeId;
 	public getId(): BigInt;
 	public getBufferId(): Buffer;
 }
-
-export default SnowflakeId;
